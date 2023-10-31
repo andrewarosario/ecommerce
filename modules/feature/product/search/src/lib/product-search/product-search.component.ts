f2d0093ce09682891f0071a6591a37fcce59fc9b
andrewarosario <andrew.arosario@gmail.com>
2023-10-30 21:02:48 -0300

    feat(product-search): integrates api

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { mockProducts } from 'product-data-access';
+import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import { ProductSearchService } from 'product-data-access';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'lib-product-search',
@@ -19,7 +27,20 @@ import { mockProducts } from 'product-data-access';
   templateUrl: './product-search.component.html',
   styleUrls: ['./product-search.component.scss'],
 })
-export class ProductSearchComponent {
-  control = new FormControl('');
-  products = mockProducts;
+export class ProductSearchComponent implements OnInit {
+  control = new FormControl('', { nonNullable: true });
+  products$!: Observable<Product[]>;
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.control.valueChanges.pipe(
+      // COLOCAR OS OPERADORES!
+      debounceTime(400),
+      distinctUntilChanged(),
+      filter((value) => value.length > 1),
+      switchMap((value) => this.productSearchService.searchByName(value))
+      // map((value) => `VALOR TRANSFORMADO: ${value}`)
+    );
+  }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LayoutModule } from 'modules/layout';
@@ -9,7 +10,12 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, LayoutModule, ProductSearchComponent],
+      imports: [
+        RouterTestingModule,
+        LayoutModule,
+        ProductSearchComponent,
+        HttpClientTestingModule,
+      ],
       declarations: [AppComponent],
     }).compileComponents();
 
