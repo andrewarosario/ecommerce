2756873fd8c361542f07d8a28a76f57fe9f1c7ee
andrewarosario <andrew.arosario@gmail.com>
2024-02-15 21:03:59 -0300

    feat(data-access-auth): add auth guard

---
+++ modules/data-access/auth/src/lib/guards/auth/auth.guard.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { AuthService } from '../../auth/auth.service';
+import { authGuard } from './auth.guard';
+
+describe('authGuard', () => {
+  it('should return true when user is not truthy', () => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+    });
+
+    TestBed.overrideProvider(AuthService, {
+      useValue: { email$: of(null) },
+    });
+    const guard = TestBed.runInInjectionContext(authGuard());
+
+    guard.subscribe((isActivated) => {
+      expect(isActivated).toBe(true);
+    });
+  });
+
+  it('should not return true when user is truthy', () => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+    });
+
+    TestBed.overrideProvider(AuthService, {
+      useValue: { email$: of('mail@mail.com') },
+    });
+    const guard = TestBed.runInInjectionContext(authGuard());
+    guard.subscribe((isActivated) => {
+      expect(isActivated).not.toBe(true);
+    });
+  });
+});
