d3c7627a06b535ec04225b5826b4cfc3602e0ead
andrewarosario <andrew.arosario@gmail.com>
2024-02-19 21:07:03 -0300

    feat(home): add log directive

--- modules/feature/home/src/lib/home.module.ts
+++ modules/feature/home/src/lib/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductCardComponent } from 'product-ui';
+import { LogDirective } from './directives/log/log.directive';
 import { HomeComponent } from './home/home.component';
 import { homeRoutes } from './lib.routes';
 
@@ -10,6 +11,7 @@ import { homeRoutes } from './lib.routes';
     CommonModule,
     RouterModule.forChild(homeRoutes),
     ProductCardComponent,
+    LogDirective,
   ],
   declarations: [HomeComponent],
 })
